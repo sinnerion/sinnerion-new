39a308685c947daa3d8972d6645a9ba8c4e011f7
sinnerion <a.dergachow@gmail.com>
2018-03-18 18:10:01 +0200

    main-nav done (js fixed)

--- src/js/common.js
+++ src/js/common.js
@@ -2,7 +2,8 @@ window.onload = function () {
   // Main-nav
   const mainNavToggle = document.querySelector('.main-nav_toggle');
   mainNavToggle.addEventListener('click', function () {
-    this.querySelectorAll('i').classList.toggle('hidden');
+    this.getElementsByTagName('i')[0].classList.toggle('hidden');
+    this.getElementsByTagName('i')[1].classList.toggle('hidden');
     document.querySelector('.main-nav').classList.toggle('main-nav__active');
   });
 };
