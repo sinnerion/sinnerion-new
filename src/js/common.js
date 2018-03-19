0b2f59c83b90ad333d5989634988f7ef98c601e5
sinnerion <a.dergachow@gmail.com>
2018-03-19 23:00:14 +0200

    portfolio upgraded; main-nav_toggle fixed

--- src/js/common.js
+++ src/js/common.js
@@ -2,8 +2,9 @@ window.onload = function () {
   // Main-nav
   const mainNavToggle = document.querySelector('.main-nav_toggle');
   mainNavToggle.addEventListener('click', function () {
-    this.getElementsByTagName('i')[0].classList.toggle('hidden');
-    this.getElementsByTagName('i')[1].classList.toggle('hidden');
+    this.querySelectorAll('i').forEach(function (value) {
+      value.classList.toggle('hidden');
+    });
     document.querySelector('.main-nav').classList.toggle('main-nav__active');
   });
 };
